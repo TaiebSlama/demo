4f9ac908c88904471a6e0083d3b4bbb37198b6ad
taiebslama <slama.taieb.contact@gmail.com>
2024-10-09 00:30:46 +0400

    first commit

---
+++ lib/src/commonMain/kotlin/MathUtils.kt
@@ -0,0 +1,10 @@
+/**
+ * Created by taieb.slama on 09/10/2024 .
+ * Copyright (c) 2023. All rights reserved.
+ */
+class MathUtils {
+
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+}
